7f567004049fc84126675bfe4ce2efaec49dd267
Sarahi Vargas <skillet.dv98@gmail.com>
2020-09-22 21:04:31 -0500

    Vargas Gutierrez Angeles Sarahi
    Practica 1.

---
+++ Vargas-Gutierrez-Angeles-Sarahi.cpp
@@ -0,0 +1,13 @@
+#Include <stdlib.h>// libreria estandar para ralizar funciones en el sistema
+#Include <stdio.h>// libreria para entrada y salida de informacion de pantalla
+
+//funcion principal
+int main () {// inicio del programa
+
+//estructura programa
+Printf("Hola mundo \n");
+Printf("Soy Sarahi \n");
+
+system ("pause");
+return 0,
+} //Fin del programa
