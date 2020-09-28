da3f0682d298b4f0df53c521c66b77516bcee409
omar-alcantar <71338701+omar-alcantar@users.noreply.github.com>
2020-09-27 22:42:33 -0500

    Castillo Salinas Adriana

---
+++ CastilloSalinasAdriana.cpp
@@ -0,0 +1,14 @@
+#include <stdlib.h> //librerias para realizar las funciones
+#include <stdio.h>
+
+//funcion princial
+
+int main() { //inicio del programa y estructura
+
+printf( "Hola ");
+printf( " Ya pude hacerlo \n");
+
+system("pause");
+return 0;
+
+}
